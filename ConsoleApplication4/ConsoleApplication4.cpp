2400f2df7f823588412b6b3bb5f6ea3a661436c9
q <q@DESKTOP-MJF0BI6>
2024-03-05 23:51:01 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main() {
+    int count = 0;
+    for (int i = 100; i <= 999; i++) {
+        int num = i;
+        int digit1 = num % 10;
+        num /= 10;
+        int digit2 = num % 10;
+        num /= 10;
+        int digit3 = num % 10;
+
+        if (digit1 == digit2 || digit2 == digit3 || digit1 == digit3) {
+            count++;
+        }
+    }
+
+    std::cout << "Количество целых чисел с двумя одинаковыми цифрами от 100 до 999:: " << count << std::endl;
+
+    return 0;
+}
